1c6f2b648263110b66e58f718b1a631ca505b916
Lalit Maganti <lalitm@google.com>
2019-04-18 13:44:50 +0100

    sanders: Enable Perfetto traced

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -159,6 +159,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gralloc.enable_fb_ubwc=1 \
     vendor.display.disable_skip_validate=1
 
+# Enable Perfetto traced
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.traced.enable=1
+
 # Wifi-Display
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
